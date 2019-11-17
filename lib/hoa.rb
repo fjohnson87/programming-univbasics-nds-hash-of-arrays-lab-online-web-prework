f808e62d0f7a275bbbf2d043ea8449b8c1ef0b36
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-17 18:33:39 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -10,5 +10,5 @@ def add_character(show, name)
   # Should return the array of the 'show' argument
   BASE_HOA 
    [:chipmunks] [3] = "Dave"
-   [:third_earthers] 
+   [:third_earthers] [3] = "" 
 end
